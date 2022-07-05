471000e68603a59091a41f32be723f7858846cf7
karansaini2 <karansaini0868@gmail.com>
2022-07-05 10:03:38 +0530

    Pushing Repository

---
+++ views/JS/custData.js
@@ -0,0 +1,62 @@
+let custData = [
+  {
+    account_no: "242121",
+    name: "Karan Saini",
+    email: "karan@gmail.com",
+    balance: "90000",
+  },
+  {
+    account_no: "027438",
+    name: "Abhay Singh",
+    email: "abhay@gmail.com",
+    balance: "60000",
+  },
+  {
+    account_no: "304190",
+    name: "Yash",
+    email: "yash@gmail.com",
+    balance: "25600",
+  },
+  {
+    account_no: "978143",
+    name: "Aditya Singhal",
+    email: "aditya@gmail.com",
+    balance: "15000",
+  },
+  {
+    account_no: "250866",
+    name: "Nikhil Dhiman",
+    email: "nikhil@gmail.com",
+    balance: "36000",
+  },
+  {
+    account_no: "563741",
+    name: "Rohit Saini",
+    email: "rohit@gmail.com",
+    balance: "10000",
+  },
+  {
+    account_no: "439432",
+    name: "Vishal Mishra",
+    email: "vishal@gmail.com",
+    balance: "78000",
+  },
+  {
+    account_no: "011216",
+    name: "Bhagvat Reddy",
+    email: "bhagvat@gmail.com",
+    balance: "65000",
+  },
+  {
+    account_no: "110572",
+    name: "Prerna Mehta",
+    email: "prerna@gmail.com",
+    balance: "30000",
+  },
+  {
+    account_no: "595384",
+    name: "Shagun Sinha",
+    email: "shagun@gmail.com",
+    balance: "25000",
+  },
+];
